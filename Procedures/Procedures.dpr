48de28557508fb85b40ebd24a5db793e804e7684
ivanProgramador <ivan-guita@hotmail.com>
2026-07-10 14:06:59 -0300

    procedures

---
+++ Procedures/Procedures.dpr
@@ -0,0 +1,14 @@
+program Procedures;
+
+uses
+  Vcl.Forms,
+  unitProcedures in 'unitProcedures.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
